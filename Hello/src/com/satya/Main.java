f732488b9cb7f658435cea642e2cc3d50d81f197
SatyasaiDirisala <49064005+SatyasaiDirisala@users.noreply.github.com>
2023-01-26 13:44:44 +0530

    Update Main.java

--- Hello/src/com/satya/Main.java
+++ Hello/src/com/satya/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello Satya");
+		System.out.println("Hello Sai First jenkins with git");
 
 	}
 
